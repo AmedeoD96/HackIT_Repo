3de02a1c3073c00f32d22817ed157492b5e202c4
lindro <stormo1398@gmail.com>
2020-05-21 11:08:53 +0200

    un vero gentiluomo risolve ogni enigma

--- Assets/Scripts/View/GameManager.cs
+++ Assets/Scripts/View/GameManager.cs
@@ -3,10 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject letter;
+
+    public GameObject correctText;
+
+    public GameObject wrongText;
 
     public GameObject cen;
 
@@ -18,9 +23,15 @@ public class GameManager : MonoBehaviour
 
     bool[] lettersGuessed;
 
+    int i = 0;
+
+    String result;
+
     // Start is called before the first frame update
     void Start()
     {
+        correctText = GameObject.Find("Correct");
+        wrongText = GameObject.Find("Wrong");
         cen = GameObject.Find("CenterOfTheScreen");
         initGame();
         initLetters();
@@ -35,6 +46,9 @@ public class GameManager : MonoBehaviour
     void initGame()
 
     {
+        correctText.SetActive(false);
+        wrongText.SetActive(false);
+
         wordToGuess = "Elephant";
         lengthOfWordToGuess = wordToGuess.Length;
         wordToGuess = wordToGuess.ToUpper();
@@ -61,22 +75,46 @@ public class GameManager : MonoBehaviour
     {
         if (Input.anyKeyDown)
         {
+            if (wrongText.activeSelf == true)
+            {
+                SceneManager.LoadScene("WordGame");
+            }
             char letterPressed = Input.inputString.ToCharArray(0,1)[0];
             int letterPressedAsInt = System.Convert.ToInt32(letterPressed);
             if (letterPressedAsInt >= 97 && letterPressed <= 122)
             {
-                for (int i = 0; i < lengthOfWordToGuess; i++)
+                if(i < lengthOfWordToGuess)
                 {
-                    if (!lettersGuessed[i])
+                    letterPressed = System.Char.ToUpper(letterPressed);
+                    GameObject.Find("letter" + (i + 1)).GetComponent<Text>().text = letterPressed.ToString();
+                    i++;
+                    result = result + letterPressed.ToString();
+                }
+                if(i == lengthOfWordToGuess)
+                {
+                    bool risultato = String.Equals(result,wordToGuess);
+                    if (risultato == true)
                     {
-                        letterPressed = System.Char.ToUpper(letterPressed);
-                        if (lettersToGuess[i] == letterPressed)
-                        {
-                            lettersGuessed[i] = true;
-                            GameObject.Find("letter" + (i + 1)).GetComponent<Text>().text = letterPressed.ToString();
-                        }
+                        correctText.SetActive(true);
+                    } else
+                    {
+                        wrongText.SetActive(true);
                     }
                 }
+
+
+                //for (int i = 0; i < lengthOfWordToGuess; i++)
+                //{
+                    //if (!lettersGuessed[i])
+                    //{
+                        //letterPressed = System.Char.ToUpper(letterPressed);
+                        //if (lettersToGuess[i] == letterPressed)
+                        //{
+                            //lettersGuessed[i] = true;
+                            //GameObject.Find("letter" + (i + 1)).GetComponent<Text>().text = letterPressed.ToString();
+                        //}
+                    //}
+                //}
             }
         }
     }
