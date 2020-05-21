6ee9ae76e8aac2b40fe6de7acfbf225bc4bef395
Amedeo D'Amelio <amedeodam@gmail.com>
2020-05-21 21:38:10 +0200

    Livello 1 - WIP

---
+++ Assets/Player.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Platformer.Mechanics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+    Rigidbody2D rb;
+    Animator anim;
+
+    private enum State {
+        idle,
+        running,
+        jumping,
+        falling,
+        hurt
+    }
+
+    private State state = State.idle;
+    private Collider2D coll;
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private float speed = 7f;
+    [SerializeField] private float jumpForce = 15f;
+    [SerializeField] private int monetine = 0;
+    [SerializeField] private Text monetineText;
+    [SerializeField] private float hurtForce = 10f;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        coll = GetComponent<Collider2D>();
+    }
+
+    private void Update() {
+        if (state != State.hurt){
+            Movement();
+        }
+        
+        AnimationState();
+        
+        anim.SetInteger("state", (int) state); //Imposto l'animazione grazie all'enum
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Monetine"){
+            Destroy(collision.gameObject);
+            monetine++;
+            monetineText.text = monetine.ToString();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag == "Enemy"){
+            Nemici enemy = other.gameObject.GetComponent<Nemici>();
+            if (state == State.falling){
+                enemy.JumpedOn();
+                Jump();
+            }
+            else{
+                state = State.hurt;
+                if (other.gameObject.transform.position.x > transform.position.x){
+                    //Nemico alla mia destra. Mi sposta a sinistra
+                    rb.velocity = new Vector2(-hurtForce, rb.velocity.y);
+                }
+                else{
+                    //Nemico alla mia sinistra. Mi sposta a destra
+                    rb.velocity = new Vector2(hurtForce, rb.velocity.y);
+                }
+            }
+            
+        }
+    }
+
+    private void Movement() {
+        float hDirection = Input.GetAxis("Horizontal");
+
+        //Movimento sinistra
+        if (hDirection < 0){
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+            transform.localScale = new Vector2(-1, 1);
+        }
+        //Movimento destra
+        else if (hDirection > 0){
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+            transform.localScale = new Vector2(1, 1);
+        }
+
+        //Salto
+        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground)){
+            Jump();
+        }
+    }
+
+    private void Jump() {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        state = State.jumping;
+    }
+
+    private void AnimationState() {
+        if (state == State.jumping){
+            if (rb.velocity.y < .1f){
+                state = State.falling;
+            }
+        }
+        else if (state == State.falling){
+            if (coll.IsTouchingLayers(ground)){
+                state = State.idle;
+            }
+        }else if (state == State.hurt){
+            if (Mathf.Abs(rb.velocity.x) < .1f){
+                state = State.idle;
+            }
+        }
+        else if (Mathf.Abs(rb.velocity.x) > 2f){
+            //Movimento
+            state = State.running;
+        }
+        else{
+            state = State.idle;
+        }
+        
+    }
+}
---
+++ Assets/WalkingEnemy.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingEnemy : Nemici {
+
+    [SerializeField] private float leftCap;
+    [SerializeField] private float rightCap;
+    [SerializeField] private float jumpLenght = 10f;
+    [SerializeField] private float jumpHeight = 15f;
+    [SerializeField] private LayerMask ground;
+    private Collider2D coll;
+    private Rigidbody2D rb;
+
+    private bool facingLeft = true;
+
+    protected override void Start() {
+        base.Start();
+        coll = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        //salto->caduta
+        if (anim.GetBool("Jumping")){
+            if (rb.velocity.y < .1){
+                anim.SetBool("Falling", true);
+                anim.SetBool("Jumping", false);
+            }
+        }
+        
+        //caduta->salto
+        if (coll.IsTouchingLayers(ground) && anim.GetBool("Falling")){
+            anim.SetBool("Falling", false);
+            
+        }
+    }
+
+    private void Move() {
+        if (facingLeft){
+            //Controllo se sono prima dell'oggetto a sinistra. Altrimenti mi giro a destra
+            if (transform.position.x > leftCap){
+                //Controllo se il nemico sta guardando a destra. Se no lo giro
+                if (transform.localScale.x != 1){
+                    transform.localScale = new Vector3(1, 1);
+                }
+
+                //Se sono a terra, salta
+                if (coll.IsTouchingLayers(ground)){
+                    //Salta
+                    rb.velocity = new Vector2(-jumpLenght, jumpHeight);
+                    anim.SetBool("Jumping", true);
+                }
+            }
+            else{
+                facingLeft = false;
+            }
+        }
+        else{
+            if (transform.position.x < rightCap){
+                //Controllo se il nemico sta guardando a destra. Se no lo giro
+                if (transform.localScale.x != -1){
+                    transform.localScale = new Vector3(-1, 1);
+                }
+
+                //Se sono a terra, salta
+                if (coll.IsTouchingLayers(ground)){
+                    //Salta
+                    rb.velocity = new Vector2(jumpLenght, jumpHeight);
+                    anim.SetBool("Jumping", true);
+                }
+            }
+            else{
+                facingLeft = true;
+            }
+        }
+    }
+
+    
+
+    
+}
