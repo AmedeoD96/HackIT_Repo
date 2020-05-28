aeebb4d957e820ab30139d7b19560fa192502726
Amedeo D'Amelio <amedeodam@gmail.com>
2020-05-29 00:22:41 +0200

    Implementato acquisto suggerimento. Miglioramenti grafici

--- Assets/Scripts/View/HintsMenuVertical.cs
+++ Assets/Scripts/View/HintsMenuVertical.cs
@@ -6,25 +6,19 @@ using UnityEngine;
 public class HintsMenuVertical : MonoBehaviour
 {
     public GameObject TestoSuggerimento;
-    private int monetine = PlayerPrefs.GetInt("monetine");
-
-
+    private int monetine;
+    
     // Start is called before the first frame update
     void Start()
     {
+        monetine = PlayerPrefs.GetInt("monetine");
         TestoSuggerimento.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
-
-    public void TaskOnClick()
+    public void SuggerimentoUno()
     {
-        if (monetine == 2){
+        if (monetine >= 2){
             TestoSuggerimento.SetActive(true);
             PlayerPrefs.SetInt("monetine", PlayerPrefs.GetInt("monetine")-2);
         }
@@ -32,7 +26,30 @@ public class HintsMenuVertical : MonoBehaviour
             TestoSuggerimento.GetComponent<TextMeshProUGUI>().text = "Non hai abbasta monete";
             TestoSuggerimento.SetActive(true);
         }
-        
+    }
+    
+    public void SuggerimentoDue()
+    {
+        if (monetine >= 3){
+            TestoSuggerimento.SetActive(true);
+            PlayerPrefs.SetInt("monetine", PlayerPrefs.GetInt("monetine")-3);
+        }
+        else{
+            TestoSuggerimento.GetComponent<TextMeshProUGUI>().text = "Non hai abbasta monete";
+            TestoSuggerimento.SetActive(true);
+        }
+    }
+    
+    public void SuggerimentoTre()
+    {
+        if (monetine >= 4){
+            TestoSuggerimento.SetActive(true);
+            PlayerPrefs.SetInt("monetine", PlayerPrefs.GetInt("monetine")-4);
+        }
+        else{
+            TestoSuggerimento.GetComponent<TextMeshProUGUI>().text = "Non hai abbasta monete";
+            TestoSuggerimento.SetActive(true);
+        }
     }
 
 }
