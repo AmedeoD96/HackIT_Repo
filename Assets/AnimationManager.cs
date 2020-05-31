03b05f6f0571369bf8f9064d429609ee37d2939f
Diciannovesimo <williamcolucci1998@gmail.com>
2020-05-31 10:18:46 +0200

    ScenaFinale
    con script vari

---
+++ Assets/AnimationManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject balloon;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        player.SetActive(true);
+        balloon.SetActive(true);
+    }
+}
---
+++ Assets/Script/RitornaAlMenuPrincipale.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RitornaAlMenuPrincipale : MonoBehaviour
+{
+    public void LoadPrincipalMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
